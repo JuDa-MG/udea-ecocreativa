e088cd97a07377402af579d5e701af8d9f7e0c12
JuDa <david.mesa2629@gmail.com>
2024-05-27 08:34:05 -0500

    segunda correcion index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
